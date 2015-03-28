482a7aec68cca38da3c57a457591542bcb8f8f7d
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2015-03-28 17:22:48 -0300

    Agrego controles para regular el filtro de colores

--- vid.py
+++ vid.py
@@ -3,48 +3,64 @@ import cv2
 import os
 
 cap = cv2.VideoCapture(0)
+FRAME_HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+FRAME_WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 
 # Load all the images in the 'img/' directory and crop to 640x480
 bg_images = []
 for bg_image_fname in os.listdir('img'):
     bg_image = cv2.imread(os.path.join('img', bg_image_fname))
-    d = bg_image.shape[0]
-    m = d/2
-    h = 480
-    w = 640
-    bg_image = bg_image[m-(h/2):m+(h/2), m-(w/2):m+(w/2)]
+    # d = bg_image.shape[0]
+    # m = d/2
+    # h = FRAME_HEIGHT
+    # w = FRAME_WIDTH
+    # bg_image = bg_image[m-(h/2):m+(h/2), m-(w/2):m+(w/2)]
     bg_images.append(bg_image)
 
 image_index = 0
 step = +1
+step_delay = 0
+delay_factor = 5
 def get_current_image():
-    global image_index, step
+    global image_index, step, step_delay
     if image_index == 0:
         step = +1
     elif image_index == len(bg_images)-1:
         step = -1
     current_image = bg_images[image_index]
-    image_index += step
+    step_delay = (step_delay + 1) % delay_factor
+    if step_delay == 0:
+        image_index += step
     return current_image
 
 
+out = None
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi', fourcc, 20.0, (FRAME_WIDTH, FRAME_HEIGHT))
 
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    if event == cv2.EVENT_RBUTTONUP:
-        print cv2.cvtColor(np.uint8([[frame[y, x]]]), cv2.COLOR_BGR2HSV)
-
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
 
 # define range of blue color in HSV
-lower_red = np.array([160, 80, 80])
-upper_red = np.array([180, 255, 255])
-red_l2 = np.array([0, 100, 150])
-red_u2 = np.array([20, 255, 255])
+color_low = np.array([160, 80, 80])
+color_high = np.array([180, 255, 255])
+cv2.namedWindow('controls')
+# create trackbars for color change
+def set_val_factory(arr, i):
+    def set_val(x):
+        arr[i] = x
+    return set_val
+
+cv2.createTrackbar('Lower H', 'controls', color_low[0], 179, set_val_factory(color_low, 0))
+cv2.createTrackbar('Lower S', 'controls', color_low[1], 255, set_val_factory(color_low, 1))
+cv2.createTrackbar('Lower V', 'controls', color_low[2], 255, set_val_factory(color_low, 2))
+
+cv2.createTrackbar('Upper H', 'controls', color_high[0], 179, set_val_factory(color_high, 0))
+cv2.createTrackbar('Upper S', 'controls', color_high[1], 255, set_val_factory(color_high, 1))
+cv2.createTrackbar('Upper V', 'controls', color_high[2], 255, set_val_factory(color_high, 2))
+
+
+#red_l2 = np.array([0, 100, 150])
+#red_u2 = np.array([20, 255, 255])
 
 while(True):
     # Capture frame-by-frame
@@ -53,9 +69,9 @@ while(True):
     # Convert BGR to HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv_frame, lower_red, upper_red)
-    m2 = cv2.inRange(hsv_frame, red_l2, red_u2)
-    mask = cv2.bitwise_or(mask, m2)
+    mask = cv2.inRange(hsv_frame, color_low, color_high)
+    #m2 = cv2.inRange(hsv_frame, red_l2, red_u2)
+    #mask = cv2.bitwise_or(mask, m2)
     kernel = np.ones((5,5),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
@@ -72,7 +88,7 @@ while(True):
 
     result = cv2.add(masked_frame, masked_bg)
     cv2.imshow('transformed', result)
-    out.write(result)
+    if out: out.write(result)
 
     cv2.imshow('image', frame)
     if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -81,5 +97,5 @@ while(True):
 
 # When everything done, release the capture
 cap.release()
-out.release()
+if out: out.release()
 cv2.destroyAllWindows()
