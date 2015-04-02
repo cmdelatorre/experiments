2a81ec6c09f9ef5873bb35952e4d2d51c6d66a56
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2015-04-02 19:53:07 -0300

    Agrego comentarios y A/B set switch para Ro

--- vid.py
+++ vid.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import numpy as np
 import cv2
 import os
@@ -8,6 +9,8 @@ FRAME_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 FRAME_WIDTH = 600  # Tamano de las imgs de hoy
 
 # Load all the images in the 'img/' directory and crop to 640x480
+# Acá estoy cargando todas las imágenes del directorio img en una lista.
+# - Se pueden tener varios fractales (listas de imágenes)
 bg_images = []
 for bg_image_fname in os.listdir('img'):
     bg_image = cv2.imread(os.path.join('img', bg_image_fname))
@@ -18,6 +21,13 @@ for bg_image_fname in os.listdir('img'):
     bg_image = bg_image[m-(h/2):m+(h/2), m-(w/2):m+(w/2)]
     bg_images.append(bg_image)
 
+
+# Esto es para hacer que el fractal avanze y retroceda.
+# Es la función que va a devolver una imagen para mezclar con cada frame.
+# - Se puede hacer que el sentido dependa de alguna variable,
+#   se puede meter aleatoriedad, se puede hacer que cambie el
+#   fractal que se está mostrando, se pueden usar imágenes más grandes
+#   que la ventana de video y mover la ventana sobre las imágenes (paneo o zoom)
 image_index = 0
 step = +1
 step_delay = 0
@@ -35,6 +45,7 @@ def get_current_image():
     return current_image
 
 
+# Esto es para grabar los dos videos (original y tocado)
 out = None
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -43,9 +54,22 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 transformed_out = cv2.VideoWriter('fractalized.avi', fourcc, 20.0, (FRAME_WIDTH, FRAME_HEIGHT))
 out = True
 
-# define range of blue color in HSV
-color_low = np.array([160, 80, 80])
-color_high = np.array([180, 255, 255])
+
+
+# Acá defino dos rangos (low,high), A y B.
+# Con el boton de switch activo uno o el otro seteo.
+A_low = np.array([0, 0, 56])
+A_high = np.array([24, 161, 255])
+
+B_low = np.array([62, 84, 36])
+B_high = np.array([179, 255, 255])
+
+# Estas variables de acá definen el rango actual en cada momento.
+color_low = A_low.copy()
+color_high = A_high.copy()
+
+
+# Acá dibujo los controles en una ventanita.
 cv2.namedWindow('controls')
 # create trackbars for color change
 def set_val_factory(arr, i):
@@ -61,42 +85,73 @@ cv2.createTrackbar('Upper H', 'controls', color_high[0], 179, set_val_factory(co
 cv2.createTrackbar('Upper S', 'controls', color_high[1], 255, set_val_factory(color_high, 1))
 cv2.createTrackbar('Upper V', 'controls', color_high[2], 255, set_val_factory(color_high, 2))
 
+def switch_toggle(x):
+    print x
+    if x == 0:
+        color_low = A_low.copy()
+        color_high = A_high.copy()
+        print color_high
+    else:
+        color_low = B_low.copy()
+        color_high = B_high.copy()
+        print color_high
+    cv2.setTrackbarPos('Lower H', 'controls', color_low[0])
+    cv2.setTrackbarPos('Lower S', 'controls', color_low[1])
+    cv2.setTrackbarPos('Lower V', 'controls', color_low[2])
 
-#red_l2 = np.array([0, 100, 150])
-#red_u2 = np.array([20, 255, 255])
+    cv2.setTrackbarPos('Upper H', 'controls', color_high[0])
+    cv2.setTrackbarPos('Upper S', 'controls', color_high[1])
+    cv2.setTrackbarPos('Upper V', 'controls', color_high[2])
 
+# create switch for ON/OFF functionality
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, 'controls',  0, 1, switch_toggle)
+
+
+# Loop principal. Funciona hasta que se aprieta 'q'
+# Se ejecuta todo para cada frame de video.
 while(True):
-    # Capture frame-by-frame
+    # Acá capturo un frame de video.
     ret, frame = cap.read()
-    frame = frame[:, : 600, :]
+    # Acá estoy modificando el ancho porque las imgs que tengo son de 600x600 y la camara 640x480.
+    frame = frame[:, : FRAME_WIDTH, :]
 
-    # Convert BGR to HSV
+    # Creación de la máscara:
+    # Primero transformo los valores de cada píxel, cambiando de (Blue, Green, Red) a HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # Threshold the HSV image to get only blue colors
+    # Del frame transformado, creo una máscara:
+    #  - Tiene 255 en los píxeles que están dentro del rango actual. 0 en los que no.
+    #    El rango actual es el valor que haya en color_low, color_high
+    #    Ese rango actual va cambiando de acuerdo a los controles.
+    #    Se puede cambiar el rango con otra cosa (arduino, etc)
     mask = cv2.inRange(hsv_frame, color_low, color_high)
-    #m2 = cv2.inRange(hsv_frame, red_l2, red_u2)
-    #mask = cv2.bitwise_or(mask, m2)
+    # Luego, aplico un filtro morfolóligo para suavizar la máscara
     kernel = np.ones((5,5),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-
+    # Acá creo una copia invertida de la máscara.
     inv_mask = cv2.bitwise_not(mask)
-    #cv2.imshow('mask', inv_mask)
-    # Bitwise-AND mask and original image
+    cv2.imshow('mask', inv_mask)  # Esto es para ver la másccara
+    # Aplico la máscara invertida en el frame original (o sea, borro lo que queda dentro)
     masked_frame = cv2.bitwise_and(frame, frame, mask=inv_mask)
     #cv2.imshow('masked_frame', masked_frame)
 
-    # Apply the inverted mask to the bg image
+    # Obtengo la imagen fractalica actual y aplico la máscara
+    # (o sea, dejo solo lo que queda dentro)
     bg_image = get_current_image()
     masked_bg = cv2.bitwise_and(bg_image, bg_image, mask=mask)
 
+    # Finalmente, sumo las dos imágenes (frame de video + fractalica)
     result = cv2.add(masked_frame, masked_bg)
+
+    # Muestro los frames de video en sus ventanitas y grabo esos frames en archivos separados.
     cv2.imshow('transformed', result)
+    cv2.imshow('image', frame)
     if out:
         original_out.write(frame)
         transformed_out.write(result)
 
-    cv2.imshow('image', frame)
+    # Si se aprieta 'q' sale del loop
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
