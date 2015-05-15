e2f09709c0c9cfeb6258e695b798f2e343197a25
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2015-05-15 00:30:24 -0300

    Try to stream video piping data to vlc

--- playground.py
+++ playground.py
@@ -1,47 +1,20 @@
-import cv2
-import numpy as np
-
-img1 = cv2.imread("img/im-julia.png")
-print "img/im-julia.png", img1.shape
-d1 = img1.shape[0]
-m1 = d1/2
-a = img1[m1-10:m1+10, m1-10:m1+10]
-mean_red = a[:,:,2].mean()
-cv2.imshow('a', img1)
+# Execute like:
+# $ python playground.py | cvlc  --demux=rawvideo --rawvid-fps=30 --rawvid-width=1280 --rawvid-height=720  --rawvid-chroma=RV24 - :sout="#transcode{vcodec=mp2v,vb=10000,fps=30,width=1280,height=720} :http{dst=:8081/desk.ts}" :no-sout-rtp-sap :no-sout-standard-sap :ttl=1 :sout-keep :clock-jitter=0 :deinterlace-mode=linear :clock-synchro=1
 
 
-b, g, red_layer = cv2.split(img1)
-_, mask_over = cv2.threshold(red_layer, mean_red+1, 255, cv2.THRESH_BINARY)
-#_, mask_below = cv2.threshold(red_layer, mean_red-1, 255, cv2.THRESH_BINARY_INV)
+import cv2, sys
 
-cv2.imshow('mo', mask_over)
-#cv2.imshow('mb', mask_below)
+source = 1
+cap = cv2.VideoCapture(source)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+FRAME_HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+FRAME_WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+sys.stderr.write("{0}, {1}".format(FRAME_WIDTH, FRAME_HEIGHT))
 
+while True:
+    frame_loaded, frame = cap.read()
+    if not frame_loaded:
+        break
 
-inv_mask = cv2.bitwise_not(mask_over)
-
-masked_r = cv2.bitwise_and(red_layer, inv_mask)
-masked_g = cv2.bitwise_and(g, inv_mask)
-masked_b = cv2.bitwise_and(b, inv_mask)
-masked_img = cv2.merge((masked_b, masked_g, masked_r))
-cv2.imshow('masked_img', masked_img)
-
-img2 = cv2.imread("img/0.20.6im-julia9.png")
-print "img/0.20.6im-julia9.png", img2.shape
-d2 = img2.shape[0]
-m2 = d2/2
-img2 = img2[m2-(d1/2):m2+(d1/2), m2-(d1/2):m2+(d1/2)]
-cv2.imshow('b', img2)
-
-b2, g2, r2 = cv2.split(img2)
-masked_r = cv2.bitwise_and(r2, mask_over)
-masked_g = cv2.bitwise_and(g2, mask_over)
-masked_b = cv2.bitwise_and(b2, mask_over)
-masked_2 = cv2.merge((masked_b, masked_g, masked_r))
-cv2.imshow('masked_2', masked_2)
-
-cv2.imshow('res', cv2.add(masked_img, masked_2))
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    sys.stdout.write(frame.tostring())
